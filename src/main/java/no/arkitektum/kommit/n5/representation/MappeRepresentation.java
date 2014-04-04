11e08ce47fd9885ae5aedfd608eb32a0da9f575f
Tor Kjetil <tor@arkitektum.no>
2014-04-04 10:16:40 +0200

    fikset systemid på mappe

--- src/main/java/no/arkitektum/kommit/n5/representation/MappeRepresentation.java
+++ src/main/java/no/arkitektum/kommit/n5/representation/MappeRepresentation.java
@@ -12,20 +12,22 @@ import no.arkitektum.kommit.n5.model.Mappe;
 @XmlRootElement(name = "mappe", namespace = Representation.N5_NAMESPACE)
 @XmlAccessorType(XmlAccessType.FIELD)
 public class MappeRepresentation extends Representation {
+	public String systemID;
 	public String mappeID;
     public String tittel;
     
 	public MappeRepresentation(String baseUri, Mappe mappe) {
 		super(baseUri);
+		systemID = mappe.systemID;
 		mappeID = mappe.mappeID;
-		tittel = mappe.mappeID;
+		tittel = mappe.tittel;
 		addLink(Representation.SELF_REL_VALUE, pathToSelf());
 		addLink("registreringer", pathToSelf() + "/registreringer");
 	}
 
 	@Override
 	public String pathToSelf() {
-		return "mappe/" + mappeID;
+		return "mappe/" + systemID;
 	}
 
 	public static List<MappeRepresentation> createFromList(String baseUri, List<Mappe> mapper) {
